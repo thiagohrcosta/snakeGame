644f6756a39a690bf1563acec5e50cc292f53d7d
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-09 13:13:38 -0300

    Criando movimentos da Snake

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ snake[0]= {
     y: 8 * box
 }
 
+// Criar variável para direção
+let direction = "right";
+
+
 // Criando função para iniciar o background
 function criarBG(){
     context.fillStyle = "lightgreen";  // Trabalha com o estilo
@@ -26,5 +30,28 @@ function criarSnake(){
     }
 }
 
-criarBG();
-criarSnake();
+function iniciarJogo(){
+    criarBG();
+    criarSnake();
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if (direction = "up") snakeY -= box;    
+    if (direction = "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX, 
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
+
