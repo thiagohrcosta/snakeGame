1b5d673ac271619f0d408b1ac4a3233bbe9befa0
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-09 13:01:38 -0300

    Criando o formato da Snake

--- script.js
+++ script.js
@@ -4,10 +4,27 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 
+// Array da Snake
+let snake = [];
+
+snake[0]= {
+    x: 8 * box,
+    y: 8 * box
+}
+
 // Criando função para iniciar o background
 function criarBG(){
     context.fillStyle = "lightgreen";  // Trabalha com o estilo
     context.fillRect(0, 0, 16 * box, 16 * box); // Desenha o retângulo do jogo
 }
 
+
+function criarSnake(){
+    for (i =0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarSnake();
