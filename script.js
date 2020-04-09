6b89a4381aa3ee8dc73be2f1b219326aa96bda11
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-09 14:10:00 -0300

    Definindo posição aleatória do alimento e GameOver

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ function drawFood(){
     context.fillRect(food.x, food.y, box, box);
 }
 
+// quando um evento ocorre, é detectado e chamado a função
 document.addEventListener('keydown', update);
 
 function update (event){
@@ -51,35 +52,45 @@ function update (event){
 
 }
 
-function iniciarJogo(){
+function iniciarJogo(){    
 
     if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarSnake();
     drawFood();
+
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
-    if (direction = "up") snakeY -= box;    
-    if (direction = "down") snakeY += box;
+    if (direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
 
-    snake.pop();
-
-    let newHead = {
-        x: snakeX, 
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop(); //pop tira o último elemento da lista
+    }else{
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
+    }
+    
+    let newHead ={
+        x: snakeX,
         y: snakeY
     }
 
-    snake.unshift(newHead);
-
+    snake.unshift(newHead); //método unshift adiciona como primeiro quadradinho da cobrinha
 }
 
 let jogo = setInterval(iniciarJogo, 100);
-
-
