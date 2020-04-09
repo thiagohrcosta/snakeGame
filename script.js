13e46da937a819425ff2ea4219a00c8bf497bb34
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-09 12:36:27 -0300

    Ajustes no script e index

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let canvas = document.getElementById("sanke");
+let canvas = document.getElementById("snake");
+
+// Trata o arquivo como sendo um plano 2D
 let context = canvas.getContext("2d");
 let box = 32;
 
+// Criando função para iniciar o background
 function criarBG(){
-    context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillStyle = "lightgreen";  // Trabalha com o estilo
+    context.fillRect(0, 0, 16 * box, 16 * box); // Desenha o retângulo do jogo
 }
 
 criarBG();
